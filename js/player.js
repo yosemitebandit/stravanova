e251d62bf26be9c799f2158dd4359511319eebb2
Matt Ball <yosemitebandit@gmail.com>
2012-07-26 19:44:01 -0700

    adds play/pause functionality and keyboard shortcuts via mousetrap.js

--- js/player.js
+++ js/player.js
@@ -1,5 +1,10 @@
 /* player.js
 */
+
+// collection of all instantiated players
+// useful for play/pause operations
+var route_players = []
+
 var Route_Player = function(route, map) {
     this.route = route;
     this.map = map
@@ -19,17 +24,30 @@ var Route_Player = function(route, map) {
         , strokeOpacity: 0.6
     });
 
+    // save in the global list of route_players
+    route_players.push(this);
+
 };
 
 Route_Player.prototype.play = function() {
+    this.is_playing = true;
+
     var self = this;
+    // should change to timeout
     this.timeout_id = window.setInterval( function() {
         self.animate_frame();
     }, 100);
 };
 
-Route_Player.prototype.pause = function() {
-    window.clearTimeout(this.timeout_id);
+Route_Player.prototype.play_pause = function() {
+    if (!this.is_playing) {
+        // was paused, play it
+        this.play();
+    } else {
+        // was playing, pause it
+        this.is_playing = false;
+        window.clearTimeout(this.timeout_id);
+    }
 };
 
 Route_Player.prototype.stop = function() {
@@ -50,6 +68,14 @@ Route_Player.prototype.animate_frame = function() {
 
     // extend polyline trace
     var path = this.polyline.getPath();
-    path.push(latest_position)
+    path.push(latest_position);
 
 };
+
+// shortcuts for play/pause
+Mousetrap.bind(['p', 'space'], function() {
+    for (var i in route_players) {
+        route_players[i].play_pause();
+    }
+});
+
