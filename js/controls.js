edecec4658cc76a17feee221d3ce015e76efe658
Matt Ball <yosemitebandit@gmail.com>
2012-07-26 22:44:32 -0700

    refactors the help overlay toggle

--- js/controls.js
+++ js/controls.js
@@ -1,11 +1,15 @@
-// shortcuts for play/pause
-Mousetrap.bind(['p', 'space'], function() {
-    for (var i in route_players) {
-        route_players[i].play_pause();
-    }
-});
+$(function() {
 
-// shortcuts for showing/hiding the help overlay
-Mousetrap.bind('h', function() {
-    $('.help-overlay').fadeToggle(100);
+    // shortcuts for play/pause
+    Mousetrap.bind(['p', 'space'], function() {
+        for (var i in route_players) {
+            route_players[i].play_pause();
+        }
+    });
+
+    // shortcuts for showing/hiding the help overlay
+    Mousetrap.bind('h', function() {
+        fade_toggle_help_overlay();
+    });
+
 });
