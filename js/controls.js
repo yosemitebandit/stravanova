d1acbed75bf993677ffef7094c4e1a3dd204f850
Matt Ball <yosemitebandit@gmail.com>
2012-07-28 09:38:51 -0700

    refactors

--- js/controls.js
+++ js/controls.js
@@ -26,6 +26,7 @@ $(function() {
 
     });
 
+    // speed controls
     Mousetrap.bind('s', function() {
         playback_speed = playback_speed / 2;
         if (playback_speed <= 1) {
@@ -41,4 +42,39 @@ $(function() {
     // init the playback speed display
     $('.playback-speed').html(playback_speed);
 
+    // add the top-right button
+    map.addControl({
+        position: 'top_right',
+        text: 'stravanova',
+        style: {
+            margin: '5px',
+            padding: '1px 6px',
+            border: 'solid 1px #717B87',
+            background: '#fff'
+        },
+        events: {
+            click: function(){
+                fade_toggle_help_overlay();
+            }
+        }
+    });
+
+    // hide the overlay when clicking the x
+    $('.close-overlay').click(function() {
+        fade_toggle_help_overlay();
+    });
+
+    // onload, fade out the overlay after a while
+    overlay_hide_timeout = window.setTimeout( function() {
+        $('.help-overlay').fadeOut(800);
+    }, 10000);
+
 });
+
+function fade_toggle_help_overlay() {
+    // clear the auto-hide timer if it hasn't fired
+    window.clearTimeout(overlay_hide_timeout);
+
+    // show/hide the div
+    $('.help-overlay').fadeToggle(100);
+}
